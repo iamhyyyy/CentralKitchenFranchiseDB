b8a19d50177d956574aa6e5301bd3d782d94f034
HarryNg59 <huyheo05092004@gmail.com>
2026-01-09 13:02:50 +0700

    add table user

--- CentralKitchenFranchiseDB.sql
+++ CentralKitchenFranchiseDB.sql
@@ -13,6 +13,47 @@ GO
 USE CentralKitchenFranchiseDB;
 GO
 
+-- USE [zPayment]
+GO
+/****** Object:  Table [dbo].[System.UserAccount]    Script Date: 04/15/25 12:23:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[System.UserAccount](
+	[UserAccountID] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](100) NOT NULL,
+	[FullName] [nvarchar](100) NOT NULL,
+	[Email] [nvarchar](150) NOT NULL,
+	[Phone] [nvarchar](50) NOT NULL,
+	[EmployeeCode] [nvarchar](50) NOT NULL,
+	[RoleId] [int] NOT NULL,
+	[RequestCode] [nvarchar](50) NULL,
+	[CreatedDate] [datetime] NULL,
+	[ApplicationCode] [nvarchar](50) NULL,
+	[CreatedBy] [nvarchar](50) NULL,
+	[ModifiedDate] [datetime] NULL,
+	[ModifiedBy] [nvarchar](50) NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_System.UserAccount] PRIMARY KEY CLUSTERED 
+(
+	[UserAccountID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[System.UserAccount] ON 
+GO
+INSERT [dbo].[System.UserAccount] ([UserAccountID], [UserName], [Password], [FullName], [Email], [Phone], [EmployeeCode], [RoleId], [RequestCode], [CreatedDate], [ApplicationCode], [CreatedBy], [ModifiedDate], [ModifiedBy], [IsActive]) VALUES (1, N'acc', N'@a', N'Accountant', N'Accountant@', N'0913652742', N'000001', 2, NULL, NULL, NULL, NULL, NULL, NULL, 1)
+GO
+INSERT [dbo].[System.UserAccount] ([UserAccountID], [UserName], [Password], [FullName], [Email], [Phone], [EmployeeCode], [RoleId], [RequestCode], [CreatedDate], [ApplicationCode], [CreatedBy], [ModifiedDate], [ModifiedBy], [IsActive]) VALUES (2, N'auditor', N'@a', N'Internal Auditor', N'InternalAuditor@', N'0972224568', N'000002', 3, NULL, NULL, NULL, NULL, NULL, NULL, 1)
+GO
+INSERT [dbo].[System.UserAccount] ([UserAccountID], [UserName], [Password], [FullName], [Email], [Phone], [EmployeeCode], [RoleId], [RequestCode], [CreatedDate], [ApplicationCode], [CreatedBy], [ModifiedDate], [ModifiedBy], [IsActive]) VALUES (3, N'chiefacc', N'@a', N'Chief Accountant', N'ChiefAccountant@', N'0902927373', N'000003', 1, NULL, NULL, NULL, NULL, NULL, NULL, 1)
+GO
+SET IDENTITY_INSERT [dbo].[System.UserAccount] OFF
+GO
+
+
 /* ============================= */
 /* 1. VEHICLE BRAND (SUB)        */
 /* ============================= */
